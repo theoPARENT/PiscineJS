f0c05ae12bda591d93eff9fb9791ff54cdbe88d9
theop <theoparent71@gmail.com>
2021-11-29 11:36:24 +0100

    Day00

--- Day00/Exo00/twoNumberSum.js
+++ Day00/Exo00/twoNumberSum.js
@@ -16,7 +16,6 @@ function twoNumberSum (array, targetSum) {
         array.forEach(function (value2, j) {
             if (i !== j) {
                 if ((value + value2) == targetSum) {
-                    console.log('La combinaison de ' + value + ' et ' + value2 + ' est bien égale à ' + targetSum)
                     let tab = [value, value2]
 
                     result.push(tab)
