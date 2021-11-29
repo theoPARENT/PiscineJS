32b4c45aa91c46c49c07c1ff09c5f479ed9c8c22
Theo <theoparent71@gmail.com>
2021-11-29 15:23:37 +0100

    Rush00

--- Day00/Exo03/firstNonRepeatedCharacter.js
+++ Day00/Exo03/firstNonRepeatedCharacter.js
@@ -1,11 +1,11 @@
-console.log(firstNonRepeatedCharacter('testabctest'))
+console.log(firstNonRepeatedCharacter('abcdcabf'))
 
 
 function firstNonRepeatedCharacter(string) {
     for (var i = 0; i < string.length; i++) {
         var c = string.charAt(i);
         if (string.indexOf(c) == i && string.indexOf(c, i + 1) == -1) {
-            return c;
+            return i;
         }
     }
     return null;
