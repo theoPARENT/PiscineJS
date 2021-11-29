15eaac08e2e14413acb7ac09517eb090c83c96d7
Theo <theoparent71@gmail.com>
2021-11-29 15:29:42 +0100

    Fix

--- Day00/Rush00/sun.js
+++ Day00/Rush00/sun.js
@@ -1,11 +1,11 @@
-console.log(sun([3,5,6,1,2,4,3], 'Est'))
+console.log(sun([3,5,6,1,2,4,3], 'Ouest'))
 
 
 function sun(buildings, direction){
     let length = buildings.length
     let result = []
     let tamp = null
-    if (direction === 'Ouest') {
+    if (direction.toLowerCase() === 'Ouest') {
         result.push(0)
         for (let i=1;i<length;i++) {
             if ( tamp !== null) {
@@ -19,7 +19,7 @@ function sun(buildings, direction){
                 tamp = i-1
             }
         }
-    } else if (direction === 'Est') {
+    } else if (direction.toLowerCase() === 'Est') {
         result.push(length-1)
         for (let i=length-2;i>=0;i--) {
             if ( tamp !== null) {
