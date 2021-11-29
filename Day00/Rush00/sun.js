3b8c047827cf609219e8e93ea647ccad75c2ae06
Theo <theoparent71@gmail.com>
2021-11-29 15:19:15 +0100

    Rush00

--- Day00/Rush00/sun.js
+++ Day00/Rush00/sun.js
@@ -1,16 +1,41 @@
-function sun(buildings, direction) {
+console.log(sun([3,5,6,1,2,4,3], 'Est'))
 
-    let count = 1;
-    let curr_max = arr[0];
-    for (let i = 1; i < n; i++) {
 
-        if (arr[i] > curr_max || arr[i] == curr_max) {
-            count++;
-            curr_max = arr[i];
+function sun(buildings, direction){
+    let length = buildings.length
+    let result = []
+    let tamp = null
+    if (direction === 'Ouest') {
+        result.push(0)
+        for (let i=1;i<length;i++) {
+            if ( tamp !== null) {
+                if (buildings[i] > buildings[tamp]) {
+                    result.push(i)
+                    tamp = null
+                }
+            } else if (buildings[i] > buildings[i-1]) {
+                result.push(i)
+            } else {
+                tamp = i-1
+            }
+        }
+    } else if (direction === 'Est') {
+        result.push(length-1)
+        for (let i=length-2;i>=0;i--) {
+            if ( tamp !== null) {
+                if (buildings[i] > buildings[tamp]) {
+                    result.push(i)
+                    tamp = null
+                }
+            } else if (buildings[i] > buildings[i+1]) {
+                result.push(i)
+            } else {
+                tamp = i+1
+            }
         }
     }
-    return count;
+    return result
 }
-let arr = [ 7, 4, 6, 2, 1 ];
-let n = arr.length;
-document.write(sun(arr, n));
+
+// Merci de ne pas effacer la ligne en dessous.
+exports.sun = sun ;
