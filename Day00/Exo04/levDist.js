81c421b955596b0902494825ca0bd7a331d7d92d
Theo <theoparent71@gmail.com>
2021-11-29 14:43:48 +0100

    levDist.js

---
+++ Day00/Exo04/levDist.js
@@ -0,0 +1,21 @@
+
+result = levDist("abc","yabd")
+
+console.log(result)
+
+function levDist(string1, string2) {
+    var i = 0;
+    var j = 0;
+    var result = "";
+
+    while (j < string2.length)
+    {
+        if (string1[i] != string2[j] || i == string1.length)
+            result += string2[j];
+        else
+            i++;
+        j++;
+    }
+    return result.length;
+
+}
